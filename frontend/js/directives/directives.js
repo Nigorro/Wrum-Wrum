6038163687d11b81bc4487e86d2555edc627b415
Nigorro <nigorro@gmail.com>
2015-04-20 22:41:23 +0300

    vk comments

---
+++ frontend/js/directives/directives.js
@@ -0,0 +1,28 @@
+app.directive('vkcomments', function($timeout) {
+  console.log('hello!');
+  return {
+    restrict: 'AE',
+    templateUrl: './js/directives/vk_comments.html',
+    replace: true,
+    scope: {
+      pageid: '=pageid',
+    },
+    link: function(scope, element, attrs) {
+      var unregister = scope.$watch('pageid', function(value) {
+        if (value) {
+
+          $timeout(function() {
+            VK.init({apiId: 4877357, onlyWidgets: true});
+             VK.Widgets.Comments("vk_comments", {limit: 15}, scope.pageid);
+          }, 400);
+        }
+      });
+
+      // var pageId = scope.pageid;
+      // console.log('!!' + scope.pageid);
+      // if (pageId) {
+        
+      // };
+    }
+  };
+});
